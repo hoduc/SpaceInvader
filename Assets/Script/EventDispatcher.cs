a3150f39097362171aa66dd2dde08135d53dd73b
hoduc <duc_ho90@yahoo.com>
2017-03-20 16:42:47 -0700

    Event Dispatcher

---
+++ Assets/Script/EventDispatcher.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class AlienDieUnityEvent : UnityEvent<Alien> {}
+
+[System.Serializable]
+public class ShootableRowEradicateUnityEvent : UnityEvent<int> {}
+public class EventDispatcher : MonoBehaviour {
+	public UnityEvent PlayerDieEvent;
+	public AlienDieUnityEvent AlienDieEvent;
+	public UnityEvent PlayerFinishedDyingEvent;
+
+	public ShootableRowEradicateUnityEvent ShootableRowEradicateEvent;
+ 	public static EventDispatcher Instance = null;    
+	 //Awake is always called before any Start functions
+	void Awake()
+	{
+		//Check if instance already exists
+		if (Instance == null)
+			//if not, set instance to this
+			Instance = this;
+		//If instance already exists and it's not this:
+		else if (Instance != this)
+			//Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
+			Destroy(gameObject);    
+		//Sets this to not be destroyed when reloading scene
+		DontDestroyOnLoad(gameObject);
+	}
+}
