e667db35c3dd27425377c2194c013b06ea6a8f67
Tian Zhou <tianzhou93@gmail.com>
2017-03-22 16:14:37 -0700

    Revert "Revert "Moving in circle(diagonal)""
    This reverts commit 18c6ff4bad267683ff64fa44ff198a14cf33732c.

--- Assets/Script/Boss.cs
+++ Assets/Script/Boss.cs
@@ -5,45 +5,73 @@ using UnityEngine;
 
 public class Boss : Ent2D {
 	private int index = 0;
-    private int indexSize = 1;
+    private int indexSize = 4;
     public float diameterX = 5.0f;
 	public float diameterY = 5.0f;
 	public float distDivider = 1.0f;
 
 	public bool startMoving = false;
 
+	public bool dev = false;
+
 	private int count = 0;
 	//testing movement
 	void Start(){
 		Init(transform.position.x, transform.position.x + diameterX, transform.position.y + diameterY/2.0f, transform.position.y - diameterY/2.0f);
+		if (dev) {
+			CreateCirc (new Vector3 (transform.position.x, transform.position.y, transform.position.z), Color.green); //lbx
+			CreateCirc (new Vector3 (transform.position.x + diameterX, transform.position.y, transform.position.z), Color.green); //rbx
+			CreateCirc (new Vector3 (transform.position.x + diameterX/2.0f, transform.position.y + diameterY/2.0f, transform.position.z), Color.green); //uby
+			CreateCirc (new Vector3 (transform.position.x + diameterX/2.0f, transform.position.y - diameterY/2.0f, transform.position.z), Color.green); //dby
+		}
 	}
 
 	public override void EntUpdate(){
-		//mover.MoveDiagonalUp(DIST_X, DIST_Y/5, RIGHT_BOUND_X, UP_BOUND_Y);
-		// count++;
 		if (startMoving && !MoveIndex (index)) {
+			if (index >= indexSize)
+				return;
 			index = ( index + 1 )%indexSize;
 		}
 	}
 
+	static void CreateCirc(Vector3 pos, Color color){
+		GameObject circ = GameObject.CreatePrimitive (PrimitiveType.Sphere);
+		circ.transform.position = pos;
+		circ.transform.localScale = new Vector3 (0.25f, 0.25f, 0.25f);
+		circ.GetComponent<Renderer> ().material.color = color;
+	}
+
 	bool MoveIndex(int index){
+		Vector3 prevPos = transform.position;
 		bool ret = false;
 		switch (index) {
 		case 0:
-			ret = mover.MoveDiagonalUpRight(DIST_X/distDivider, DIST_Y/distDivider, RIGHT_BOUND_X/2, UP_BOUND_Y/2);
+			ret = mover.MoveDiagonalUpRight(DIST_X/distDivider, DIST_Y/distDivider, LEFT_BOUND_X + diameterX/2.0f, UP_BOUND_Y);
+			if(ret)
+				Debug.Log("up-right!");
 			break;
 		case 1:
-			ret = mover.MoveDiagonalDownRight(DIST_X/distDivider, DIST_Y/distDivider, RIGHT_BOUND_X, DOWN_BOUND_Y/2);
+			//Debug.Log ("here?");
+			ret = mover.MoveDiagonalDownRight(DIST_X/distDivider, DIST_Y/distDivider, RIGHT_BOUND_X, DOWN_BOUND_Y - diameterY/2.0f);
+			if(ret)
+				Debug.Log ("down-right!");	
 			break;
-        case 2:
-			ret = mover.MoveDiagonalDownLeft(DIST_X / distDivider, DIST_Y/distDivider, LEFT_BOUND_X/2, DOWN_BOUND_Y/2);
+		case 2:
+			ret = mover.MoveDiagonalDownLeft(DIST_X/distDivider, DIST_Y/distDivider, LEFT_BOUND_X + diameterX/2.0f, DOWN_BOUND_Y - diameterY/2.0f);
+			if(ret)
+				Debug.Log ("down-left!");
             break;
-        case 3:
-			ret = mover.MoveDiagonalUpLeft(DIST_X / distDivider, DIST_Y/distDivider, LEFT_BOUND_X/2, UP_BOUND_Y/2);
+		case 3:
+			
+			ret = mover.MoveDiagonalUpLeft(DIST_X/distDivider, DIST_Y/distDivider, LEFT_BOUND_X, UP_BOUND_Y - diameterY/2.0f);
+			if(ret)
+				Debug.Log ("up-left!");
             break;
 
         }
-
+		if(dev && ret){
+			CreateCirc (prevPos, Color.red);
+		}
 		return ret;
 	}
 }
